405804f9d74ce02653d40214aaa288fc49776eee
pgorgita <pgorgita@gmail.com>
2024-11-13 11:19:11 +0100

    hotfix as in: https://stackoverflow.com/a/78594913/6403588

--- lib/unicorn/http_response.rb
+++ lib/unicorn/http_response.rb
@@ -66,7 +66,16 @@ module Unicorn::HttpResponse
           # key in Rack < 1.5
           hijack = value
         else
-          append_header(buf, key, value)
+          case value
+          when Array # Rack 3
+            value.each { |v| buf << "#{key}: #{v}\r\n" }
+          when /\n/ # Rack 2
+            # avoiding blank, key-only cookies with /\n+/
+            value.split(/\n+/).each { |v| buf << "#{key}: #{v}\r\n" }
+          else
+            buf << "#{key}: #{value}\r\n"
+          end
+          #append_header(buf, key, value)
         end
       end
       if !hijack && !term && req.chunkable_response?
